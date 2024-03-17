64e301e8d62d0f11839d3bd9968f0672bf7887dc
Die Krummy <egorich.mih05@gmail.com>
2024-03-17 13:44:28 +0700

    second commit triangle

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        //Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите длину первой стороны треугольника:");
+        /*System.out.println("Введите длину первой стороны треугольника:");
         double side1 = in.nextDouble();
         System.out.println("Введите длину второй стороны треугольника:");
         double side2 = in.nextDouble();
         System.out.println("Введите длину третьей стороны треугольника:");
-        double side3 = in.nextDouble();
+        double side3 = in.nextDouble();*/
+        double side1 = 12;
+        double side2 = 12;
+        double side3 = 23;
 
         Triangle triangle = new Triangle(side1, side2, side3);
         System.out.println("Периметр треугольника: " + triangle.getPerimeter());
